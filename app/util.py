0f9bafd8b96ae1ed4f3c76410d896ba133902164
vladimir <grun_gespenst@tut.by>
2022-09-30 20:27:01 +0300

    dict added

--- app/util.py
+++ app/util.py
@@ -10,7 +10,7 @@ def count(text):
             if len(splt) > 2:
                 result = '-1'
             else:
-                result = int(splt[0]) / int(splt[1])
+                result = round(int(splt[0]) / int(splt[1]), 8)
         elif '*' in text:
             splt = text.split('*')
             if len(splt) > 2:
@@ -39,31 +39,13 @@ def count(text):
 
 
 def say(text):
+    meaning = {'1': 'one', '2': 'two','3': 'three','4': 'four','5': 'five',
+               '6': 'six', '7': 'seven', '8': 'eight', '9': 'nine',
+               '0': 'zero', '.': 'dot', '-': 'minus',
+              }
     for i in text:
-        if i == '1':
-            number = vlc.MediaPlayer(''.join(("app/", "media/", "one.wav")))
-        elif i == '2':
-            number = vlc.MediaPlayer(''.join(("app/", "media/", "two.wav")))
-        elif i == '3':
-            number = vlc.MediaPlayer(''.join(("app/", "media/", "three.wav")))
-        elif i == '4':
-            number = vlc.MediaPlayer(''.join(("app/", "media/", "four.wav")))
-        elif i == '5':
-            number = vlc.MediaPlayer(''.join(("app/", "media/", "five.wav")))
-        elif i == '6':
-            number = vlc.MediaPlayer(''.join(("app/", "media/", "six.wav")))
-        elif i == '7':
-            number = vlc.MediaPlayer(''.join(("app/", "media/", "seven.wav")))
-        elif i == '8':
-            number = vlc.MediaPlayer(''.join(("app/", "media/", "eight.wav")))
-        elif i == '9':
-            number = vlc.MediaPlayer(''.join(("app/", "media/", "nine.wav")))
-        elif i == '0':
-            number = vlc.MediaPlayer(''.join(("app/", "media/", "zero.wav")))
-        elif i == '-':
-            number = vlc.MediaPlayer(''.join(("app/", "media/", "minus.wav")))
-        elif i == '.':
-            number = vlc.MediaPlayer(''.join(("app/", "media/", "dot.wav")))
+        treck = meaning.get(i)
+        number = vlc.MediaPlayer(''.join(("app/", "media/", treck, ".wav")))
         number.audio_set_volume(100)
         number.play()
         sleep(1)
