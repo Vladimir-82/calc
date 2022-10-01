9ce49403d5902921d5815a07709c03f2475aabe8
vladimir <grun_gespenst@tut.by>
2022-10-01 14:20:04 +0300

    eng_complete

--- app/views.py
+++ app/views.py
@@ -7,8 +7,10 @@ def index(request):
     if request.method == 'POST':
         action = request.POST['meaning']
         answer_num = count(action)
+        if str(answer_num).endswith('.0'):
+            answer_num = int(answer_num)
         context = {"answer": str(answer_num)}
-        say(str(round(answer_num, 8)))
+        say(str(answer_num))
         return render(request, 'app/index.html', context=context)
     else:
         context = {"answer": "answer"}
